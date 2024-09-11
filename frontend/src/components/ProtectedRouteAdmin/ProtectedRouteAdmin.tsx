91606abb9fc3a9d1e5915ccbf8273bc8cdaaefac
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-09-11 12:04:06 +0400

    remove comments from protectecomponent

--- frontend/src/components/ProtectedRouteAdmin/ProtectedRouteAdmin.tsx
+++ frontend/src/components/ProtectedRouteAdmin/ProtectedRouteAdmin.tsx
@@ -1,41 +1,39 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAppSelector } from "../../hooks/authHooks";
-// import { jwtDecode, JwtPayload } from "jwt-decode";
+// import { useAppSelector } from "../../hooks/authHooks";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
-// interface JwtPayloadType extends JwtPayload {
-//     "cognito:groups"?: string[];
-// }
+interface JwtPayloadType extends JwtPayload {
+    "cognito:groups"?: string[];
+}
 
 interface ProtectedRouteAdminProps {
     children?: React.ReactNode;
 }
 
 const ProtectedRouteAdmin: React.FC<ProtectedRouteAdminProps> = ({ children }) => {
-    // const [roleState, setRoleState] = useState<string | undefined>("");
-    // const token = localStorage.getItem("userToken");
+    const [roleState, setRoleState] = useState<string | undefined>("");
+    const token = localStorage.getItem("userToken");
     const location = useLocation();
-    const {users} = useAppSelector((state)=> state.users);
-    console.log(users[0].role);
-    
+    // const {role} = useAppSelector((state)=> state.users.users);
 
-    // useEffect(() => {
-    //     if (token) {
-    //         const decodedData = jwtDecode<JwtPayloadType>(token);
-    //         const role = decodedData["cognito:groups"]?.[0];
-    //         setRoleState(role);
-    //         console.log(role);
-    //     }
-    // }, []);
+    useEffect(() => {
+        if (token) {
+            const decodedData = jwtDecode<JwtPayloadType>(token);
+            const role = decodedData["cognito:groups"]?.[0];
+            setRoleState(role);
+            console.log(role);
+        }
+    }, []);
 
-    // if (role == "") return <Navigate to="/" />;
+    if (!token) return <Navigate to="/" />;
 
-    // if (role) {
+    if (roleState?.includes("ADMINGroup")) {
         return children;
-    // }
-    // else {
-    //     <Navigate to="/" state={{ from: location }} replace />
-    // }
+    }
+    else {
+        <Navigate to="/" state={{ from: location }} replace />
+    }
 };
 
 export default ProtectedRouteAdmin;
